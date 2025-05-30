ef491d5e2339628784a59716ea68f529df7722fb
PanchalKeyur0806 <panchalkeyur694@outlook.com>
2025-05-30 10:56:08 +0530

    added some 'comments' and 'error handling' in '/controllers/orderController.js' file

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -3,17 +3,24 @@ import dotenv from "dotenv";
 
 import Cart from "../models/cartModel.js";
 import Books from "../models/booksModel.js";
-import catchAsync from "../../urlshortener/utils/catchAsync.js";
 import User from "../models/userModel.js";
+import Reservation from "../models/reservationModel.js";
+
+import catchAsync from "../../urlshortener/utils/catchAsync.js";
+import AppError from "../utils/AppError.js";
 
 dotenv.config();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
+// check out session
 const createCheckoutSession = catchAsync(async (req, res, next) => {
   const loggedInUser = req.user.id;
 
   let user = await User.findById(loggedInUser);
   let cart = await Cart.findOne({ user: loggedInUser }).populate("items.book");
+
+  if (!user) return next(new AppError("User not found", 404));
+  if (!cart) return next(new AppError("cart not found", 404));
 
   const line_items = cart.items.map((item) => ({
     price_data: {
@@ -26,6 +33,7 @@ const createCheckoutSession = catchAsync(async (req, res, next) => {
     quantity: item.quantity,
   }));
 
+  // create a session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     mode: "payment",
@@ -37,6 +45,7 @@ const createCheckoutSession = catchAsync(async (req, res, next) => {
     cancel_url: `${req.protocol}://${req.get("host")}/orders/cancel`,
   });
 
+  // return the response
   res.status(200).json({
     status: "success",
     url: session.url,
@@ -44,12 +53,14 @@ const createCheckoutSession = catchAsync(async (req, res, next) => {
   });
 });
 
+// success controller
 const success = catchAsync(async (req, res, next) => {
   const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
 
   res.redirect("/");
 });
 
+// cancel controller
 const cancel = catchAsync(async (req, res, next) => {
   res.redirect("/");
 });
